181eff6df53869b2faba4bad48cdc3d78d85192e
David Condor <wdavidcl@hotmail.com>
2020-04-02 11:48:39 +0200

    2 steps

--- evaluate_model.py
+++ evaluate_model.py
@@ -4,9 +4,10 @@ from keras.models import load_model
 from keras.models import model_from_json
 import json
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
-step = 1
+step = 2
 
 
 with open('model_in_json.json','r') as f:
@@ -16,13 +17,24 @@ model = model_from_json(model_json)
 model.load_weights('model_weights.h5')
 
 model.summary()
-# load dataset
+# load dataset -artificial
 u_test = 0*np.ones(200)
 u_test = np.concatenate((u_test,5*np.ones(200)),axis=0)
 u_test = np.concatenate((u_test,2.5*np.ones(200)),axis=0)
 u_test = np.concatenate((u_test,1*np.ones(200)),axis=0)
 u_test = np.concatenate((u_test,4.5*np.ones(200)),axis=0)
+
+# load dataset - simulink
+
+data = pd.read_csv('heater.csv')
+y_k = data.iloc[:, 0:1]
+u_k = data.iloc[:, 1:2]
+
+values = u_k.values
+u_test = values[:,:]
+
 u_test = np.append(u_test,np.repeat(u_test[-1,],step))
+
 #print("original", u_test, u_test.shape)
 for w in range(0,len(u_test)):
     #print("primera m:",u_test, u_test.shape )
@@ -43,9 +55,10 @@ for w in range(0,len(u_test)):
 try:
     plt.plot(np.arange(len(u_test)), u_test, 'b')
     plt.plot(np.arange(len(u_test)), prediction,'r')
+    plt.plot(np.arange(len(y_k)), y_k,'g')
 finally:
     pass
 plt.show()
-
+print(len(y_k),len(prediction))
 #score = model.evaluate(X, Y, verbose=0)
 #print("%s: %.2f%%" % (model.metrics_names[1], score[1]*100))
--- plant_ident.py
+++ plant_ident.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import json
 
-step = 1
+step = 2
 
 def convertToMatrix(y_k,u_k, step):
     X, Y =[], []
@@ -40,10 +40,10 @@ model.add(SimpleRNN(units=32, input_shape=(2,step), activation="relu"))
 model.add(Dense(16, activation="relu")) 
 model.add(Dense(8, activation="relu")) 
 model.add(Dense(1))
-model.compile(loss='mean_squared_error', optimizer='rmsprop')
+model.compile(loss='mean_squared_error', optimizer='adam')
 model.summary()
 
-model.fit(trainX,trainY, epochs=500, batch_size=8, verbose=2)
+model.fit(trainX,trainY, epochs=50, batch_size=8, verbose=2)
 prediction = model.predict(trainX)
 
 test = np.append(test,np.repeat(test[-1,],step))
@@ -62,28 +62,30 @@ plt.grid()
 plt.plot(index, u_k, 'g') # plotting t, a separately 
 plt.plot(index, y_k, '*')
 plt.plot(index, prediction, 'r')
-
-offset = len(index)
-u_test = 0*np.ones(200)
-u_test = np.concatenate((u_test,5*np.ones(200)),axis=0)
-u_test = np.concatenate((u_test,2.5*np.ones(200)),axis=0)
-u_test = np.concatenate((u_test,1*np.ones(200)),axis=0)
-u_test = np.concatenate((u_test,4.5*np.ones(200)),axis=0)
-u_test = np.append(u_test,np.repeat(u_test[-1,],step))
-#print("original", u_test, u_test.shape)
-for w in range(0,len(u_test)):
-    #print("primera m:",u_test, u_test.shape )
-    u = [prediction[-1],u_test[w]]
-    testX = np.array(u)
-    testX = testX.reshape(1, 2, step)
-    new_y = model.predict(testX)
-    prediction = np.concatenate((prediction,new_y),axis=0)
-    #print(len(u_test),len(index),len(prediction))
+try: 
+    offset = len(index)
+    u_test = 0*np.ones(200)
+    u_test = np.concatenate((u_test,5*np.ones(200)),axis=0)
+    u_test = np.concatenate((u_test,2.5*np.ones(200)),axis=0)
+    u_test = np.concatenate((u_test,1*np.ones(200)),axis=0)
+    u_test = np.concatenate((u_test,4.5*np.ones(200)),axis=0)
+    u_test = np.append(u_test,np.repeat(u_test[-1,],step))
+    #print("original", u_test, u_test.shape)
+    for w in range(0,len(u_test)):
+        #print("primera m:",u_test, u_test.shape )
+        u = [prediction[-1],u_test[w]]
+        testX = np.array(u)
+        testX = testX.reshape(1, 2, step)
+        new_y = model.predict(testX)
+        prediction = np.concatenate((prediction,new_y),axis=0)
+        #print(len(u_test),len(index),len(prediction))
+except Exception:
+    pass
 
 try:
     plt.plot(np.arange(len(u_test))+offset, u_test, 'b')
     plt.plot(np.arange(len(u_test))+offset, prediction[offset:],'r')
-finally:
+except Exception:
     pass
 plt.show()
 
